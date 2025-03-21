520d15cf4e96014ec2767d6b3888977d4b11c519
anhnguyen777 <22211tt2678@mail.tdc.edu.vn>
2025-03-21 17:09:46 +0700

    cau truc brand template

---
+++ storage/framework/views/f9a5685d46f4bc0657c9db15fad508b9.php
@@ -0,0 +1,33 @@
+<?php $__env->startSection('content'); ?>
+    <main class="login-form">
+        <div class="container">
+            <div class="row justify-content-center">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $__currentLoopData = $users; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $user): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            <tr>
+                                <th><?php echo e($user->id); ?></th>
+                                <th><?php echo e($user->name); ?></th>
+                                <th><?php echo e($user->email); ?></th>
+                                <th>
+                                    <a href="<?php echo e(route('user.readUser', ['id' => $user->id])); ?>">View</a> |
+                                    <a href="<?php echo e(route('user.updateUser', ['id' => $user->id])); ?>">Edit</a> |
+                                    <a href="<?php echo e(route('user.deleteUser', ['id' => $user->id])); ?>">Delete</a>
+                                </th>
+                            </tr>
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </main>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('dashboard', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH D:\laptrinhweb2\resources\views/crud_user/list.blade.php ENDPATH**/ ?>
